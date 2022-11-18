450c7d3f1dd1ce7b7bc24adf28ea3f7d4269c39e
Leontking <abhi3004aarav8006@gmail.com>
2022-11-18 18:55:26 +0530

    HEllo world
    FUCKK

--- posts.php
+++ posts.php
@@ -5,6 +5,7 @@
   <link href="/sumthing.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <body>
+  <p>Made With <strong class="importanttext"><a class="importanttext" href="https://github.com/Leontking/Simple-Blog-post-software">PHP</a></strong></p>
 <?php
 $servername = "localhost";
 $username = "root";
